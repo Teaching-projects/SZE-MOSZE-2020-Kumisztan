8294e159e7f112fdb2596465b1cd6c1525ada0e6
Győrvári Péter <gyorvaripeter1996@gmail.com>
2020-09-13 23:35:43 +0200

    code backbone

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+static void (string name1, int dmg1, int heal1, string name2, int dmg2, int heal2)
+{
+    
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
