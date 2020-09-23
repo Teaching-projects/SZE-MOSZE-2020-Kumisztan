86272f7220fa838ff9ddbce97b65382c95359002
Győrvári Péter <gyorvaripeter1996@gmail.com>
2020-09-23 21:02:40 +0200

    prepare main to receive json file

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 #include "Fighters.h"
 
-void Result(Fighters& winner, Fighters& loser) 
+void Result(Fighters& winner) 
 {
-	std::cout << loser.getID() << " died. " << winner.getID() << " wins.\n";
+	std::cout <<winner.getID() << " wins. Remaining HP: " << winner.getHP() << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-    Fighters A(argv[1], std::stoi(argv[2]), std::stoi(argv[3]));
-	Fighters B(argv[4], std::stoi(argv[5]), std::stoi(argv[6]));
-
-	std::cout << A << B;
-
-	while (A.getHP() > 0)
+    Fighters A = Fighters::parseUnit(argv[1]);
+	Fighters B = Fighters::parseUnit(argv[2]);
+	try
 	{
-		std::cout << A.getID() << " -> " << B.getID() << std::endl;
-		A.Fight(B);
-		std::cout << A << B;
-
-		if (B.getHP() == 0) { break; }
-
-		std::cout << B.getID() << " -> " << A.getID() << std::endl;
-		B.Fight(A);
-		std::cout << A << B;
+		while (A.getHP() > 0)
+		{
+			A.Fight(B);
+			if (B.getHP() == 0) { break; }
+			B.Fight(A);
+		}
+		(A.getHP() > 0) ? Result(A) : Result(B);
 	}
-
-	(A.getHP() > 0) ? Result(A, B) : Result(B, A);
-
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
