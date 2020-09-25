e469c73ac257f20072730e07e180a031cc7ec311
Győrvári Péter <gyorvaripeter1996@gmail.com>
2020-09-25 20:06:50 +0200

    fix diff problem

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 void Result(Fighters& winner) 
 {
-	std::cout <<winner.getID() << " wins. Remaining HP: " << winner.getHP() << std::endl;
+	std::cout <<winner.getID() << " wins. Remaining HP: " << winner.getHP() << "\n";
 }
 
 int main(int argc, char *argv[])
