9a753cb924797d239ed4da5c930973aec04f18f4
unknown <danivr0427@gmail.com>
2020-09-25 16:50:30 +0200

    run_test.sh correction

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,15 @@ IFS = $\'n'
 
 rm output.txt
 
-for i in 'cat Fights.txt'; do
-    echo "$i" | $1 >> $2
-done
+./a.out Petyero Vargussen >> output.txt
+./a.out Petyero Umbg >> output.txt
+./a.out Vargussen Petyero >> output.txt
+./a.out Vargussen Umbg >> output.txt
+./a.out Umbg Petyero >> output.txt
+./a.out Umbg Vargussen >> output.txt
+
+diff=$(diff  output.txt good_output.txt)
+if [ "$diff" ]
+then 
+  echo "Wrong output!"
+fi
