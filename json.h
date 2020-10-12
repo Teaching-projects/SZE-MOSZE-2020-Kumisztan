6be9802ba7435c715e8e9dd0fdb86db76c6f98de
Győrvári Péter <gyorvaripeter1996@gmail.com>
2020-10-13 00:08:12 +0200

    json.h build

---
+++ json.h
@@ -0,0 +1,14 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+class Parser{
+   public:
+        static std::map<std::string, std::string> Parse_string(const std::string& filename);
+        static std::map<std::string, std::string> Parse_stream(std::istream& filename);   
+};
+
+#endif
