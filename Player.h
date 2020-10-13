227e15ab2c6a52d6f5834e10a61e6e8cb7ce85af
unknown <danivr0427@gmail.com>
2020-10-13 15:55:42 +0200

    Correct files for new task

---
+++ Player.h
@@ -0,0 +1,50 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream> 
+#include <fstream>
+class Player
+{
+    const std::string ID;
+    double maxhp; 
+    int hp;
+    double dmg;
+    int xp = 0;
+    int level = 1;
+public:
+    Player(std::string _ID, int _maxhp, int _dmg): ID(_ID), maxhp(_maxhp), dmg(_dmg), hp(_maxhp)
+    {}
+    ~Player()
+    {}
+    
+    static Player parseUnit(const std::string &jsonfile);
+
+    std::string getID() const
+    { 
+        return ID;
+    }
+   
+    int getHP() const
+    {
+        return hp;
+    }
+    
+    int getDmg() const
+    {
+        return dmg;
+    }
+
+    int getXP() const
+    {
+        return xp;
+    }
+
+    int getLevel() const
+    {
+        return level;
+    }
+
+    void Fight(Player &enemy);
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include "Fighters.h"
+#include "Player.h"
 
-void Result(Fighters& winner) 
+/*void Result(Fighters& winner) 
 {
 	std::cout <<winner.getID() << " wins. Remaining HP: " << winner.getHP() << "\n";
+}*/
+
+void Result(Player& winner) 
+{
+	std::cout <<winner.getID() << " wins. Remaining HP: " << winner.getHP() << ". Level: " << winner.getLevel() << "\n";
 }
 
 int main(int argc, char *argv[])
 {
- 	try
+
+ 	/*try
 	{  
 		Fighters A = Fighters::parseUnit(argv[1]);
 		Fighters B = Fighters::parseUnit(argv[2]);
+			
+		while (A.getHP() > 0)
+			{
+ 				A.Fight(B);
+   				if (B.getHP() == 0) {Result(A); break;}
+				B.Fight(A);
+				if (A.getHP() == 0) {Result(B); break;}
+			}
+   	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}*/
+
+	try
+	{  
+		Player A = Player::parseUnit(argv[1]);
+		Player B = Player::parseUnit(argv[2]);
 			
 		while (A.getHP() > 0)
 			{
